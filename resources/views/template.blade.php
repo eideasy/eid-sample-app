4b91595c8d339cc6343d9d61aff9f8675c4435bc
Margus Pala <margus.pala@gmail.com>
2020-07-22 17:19:30 +0300

    Upgrade jquery

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -16,7 +16,7 @@
         @yield('content')
     </div>
 </div>
-<script src="/js/vendor/jquery-3.5.1-slim.min.js"></script>
+<script src="/js/vendor/jquery-3.5.1.min.js"></script>
 <script src="/js/vendor/popper.min.js"></script>
 <script src="/js/vendor/bootstrap.min.js"></script>
 <script src="/js/vendor/toastr.min.js"></script>
