f0f9512a5c888bfca2519363973549a4c42f9207
Margus Pala <margus.pala@gmail.com>
2021-01-06 12:16:44 +0200

    EID_REDIRECT_URI .env change

--- config/eideasy.php
+++ config/eideasy.php
@@ -5,6 +5,6 @@ return [
     'secret'        => env('EID_SECRET', '56RkLgZREDi1H0HZAvzOSAVlxu1Flx41'),
     'api_url'       => env('EID_API_URL', 'https://test.eideasy.com'),
     'card_domain'   => env('EID_CARD_DOMAIN', '.test.eideasy.localhost'),
-    'redirect_uri'  => env('REDIRECT_URI', 'http://eideasy-test.localhost'),
+    'redirect_uri'  => env('EID_REDIRECT_URI', 'http://eideasy-test.localhost'),
     'pades_api_uri' => env('EID_PADES_API_URL', 'https://detached-pdf.eideasy.com'),
 ];
