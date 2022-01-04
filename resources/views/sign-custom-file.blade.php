f3fd2362c2d59dd21877e7be4e45caf0be6a431a
BanalitoRaulito <60259676+BanalitoRaulito@users.noreply.github.com>
2022-01-04 10:51:54 -0600

    FEATURE: Validate pdf to be one pdf; API-381

--- resources/views/sign-custom-file.blade.php
+++ resources/views/sign-custom-file.blade.php
@@ -5,6 +5,9 @@
         Upload the file to be signed
     </h1>
 
+    @if(Session::has('message'))
+        <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+    @endif
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
