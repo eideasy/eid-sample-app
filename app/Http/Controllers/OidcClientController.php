b3846c34edcc101c1eaa1483f7c5b596de0121fd
Oluwatobi Omisakin <oluwatobi.omisakin@eideasy.com>
2023-11-24 21:12:19 +0200

    feat: Use a different package for the oidc flow

--- app/Http/Controllers/OidcClientController.php
+++ app/Http/Controllers/OidcClientController.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Facile\OpenIDClient\Client\ClientInterface;
+use Facile\OpenIDClient\Service\AuthorizationService;
+use Facile\OpenIDClient\Service\Builder\AuthorizationServiceBuilder;
+use Facile\OpenIDClient\Token\IdTokenVerifierBuilder;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Stream;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Jumbojett\OpenIDConnectClient;
 
 class OidcClientController
@@ -12,17 +21,46 @@ class OidcClientController
     {
     }
 
-    public function startAuthentication(): void
+    public function startAuthentication(ClientInterface $client): mixed
     {
         // Redirection happens in the library.
-        $this->oidc->authenticate();
+//        $this->oidc->authenticate();
+        $authorizationService = (new AuthorizationServiceBuilder())->build();
+        $redirectAuthorizationUri = $authorizationService->getAuthorizationUri(
+            $client,
+            [
+                'scope' => implode(' ', ['openid', 'profile']),
+                'response_type' => 'code',
+                'nonce' => '1234567890',
+                'state' => '1234567890',
+            ]
+        );
+        Log::info('Redirecting to ' . $redirectAuthorizationUri);
+
+        return redirect($redirectAuthorizationUri);
     }
 
-    public function returnCallback(): \Illuminate\View\View
+    public function returnCallback(Request $request, ClientInterface $client): \Illuminate\View\View
     {
-        // authenticate() method is necessary because the redirection is handled in it.
-        $this->oidc->authenticate();
-        $userData = collect($this->oidc->getVerifiedClaims());
+        $tokenVerifier = new IdTokenVerifierBuilder();
+        // Set leeway to 1 second because we might have a float timestamp instead of int.
+        $tokenVerifier->setClockTolerance(1);
+
+        $builder = new AuthorizationServiceBuilder();
+        $authorizationService = $builder
+            ->setIdTokenVerifierBuilder($tokenVerifier)
+            ->build();
+
+        $tokenSet = $authorizationService
+            ->callback($client, $request->all());
+
+        $idToken = $tokenSet->getIdToken();
+
+        if ($idToken) {
+            $userData = $tokenSet->claims(); // IdToken claims
+        } else {
+            throw new \RuntimeException('Unauthorized');
+        }
 
         return view('welcome', ['authorizeUri' => '', "userData" => $userData]);
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Providers;
 
+use Facile\JoseVerifier\JWK\JwksProviderBuilder;
+use Facile\OpenIDClient\Client\ClientBuilder;
+use Facile\OpenIDClient\Client\ClientInterface;
+use Facile\OpenIDClient\Client\Metadata\ClientMetadata;
+use Facile\OpenIDClient\Issuer\IssuerBuilder;
+use Facile\OpenIDClient\Issuer\Metadata\Provider\MetadataProviderBuilder;
+use Illuminate\Cache\Repository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Jumbojett\OpenIDConnectClient;
+use Psr\SimpleCache\CacheInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,58 +38,50 @@ class AppServiceProvider extends ServiceProvider
             'card_domain' => config('eideasy.card_domain'),
             'api_url' => config('eideasy.api_url'),
         ]);
-        $this->app->extend(OpenIDConnectClient::class, function ($service, $app) {
-            // Fixes https://github.com/jumbojett/OpenID-Connect-PHP/pull/282 until its merged.
-            return $this->extendOpenIdConnectClient();
-        });
+
+        $this->prepareOidcClient();
 
     }
 
-    private function extendOpenIdConnectClient(): OpenIDConnectClient
+    private function prepareOidcClient(): void
     {
-        return new class extends OpenIDConnectClient {
-            public function __construct()
-            {
-                parent::__construct(
-                    config('eideasy.api_url'),
-                    config('eideasy.client_id'),
-                    config('eideasy.secret'),
-                );
-                $this->setResponseTypes(['code']);
-                $this->setAllowImplicitFlow(false);
-                $this->setRedirectURL(route('oidc.callback'));
-                // On non-production environments, we don't want to verify the SSL certificate.
-                //$oidc->setVerifyHost(false);
-                //$oidc->setVerifyPeer(false);
-                $this->addScope(['openid', 'profile']);
-                $this->setTokenEndpointAuthMethodsSupported([
-                    // We'll switch to client_secret_jwt once its implemented.
-                    //'client_secret_jwt',
-                    'client_secret_post'
-                ]);
-            }
+        $this->app->bind(ClientInterface::class, function($app) {
+            // Use a cache to avoid to fetch issuer configuration and keys on every request.
+            /** @var CacheInterface|Repository $cache */
+            $cache = $app->get(Repository::class); // get your simple-cache implementation
+            $metadataProviderBuilder = (new MetadataProviderBuilder())
+                ->setCache($cache)
+                ->setCacheTtl(60 * 60 * 24); // Cache metadata for 1 day
 
-            // Fixes https://github.com/jumbojett/OpenID-Connect-PHP/pull/282 until its merged.
-            protected function verifyJWTclaims($claims, $accessToken = null)
-            {
-                if (isset($claims->at_hash) && isset($accessToken)) {
-                    if (isset($this->getIdTokenHeader()->alg) && $this->getIdTokenHeader()->alg !== 'none') {
-                        $bit = substr($this->getIdTokenHeader()->alg, 2, 3);
-                    } else {
-                        // TODO: Error case. throw exception???
-                        $bit = '256';
-                    }
-                    $len = ((int)$bit) / 16;
-                    $expected_at_hash = $this->urlEncode(substr(hash('sha' . $bit, $accessToken, true), 0, $len));
-                }
-                return (($this->validateIssuer($claims->iss))
-                    && (($claims->aud === $this->getClientID()) || in_array($this->getClientID(), $claims->aud, true))
-                    && (!isset($claims->nonce) || $claims->nonce === $this->getNonce())
-                    && (!isset($claims->exp) || (in_array(gettype($claims->exp), ['integer', 'double']) && ($claims->exp >= time() - $this->getLeeway())))
-                    && (!isset($claims->nbf) || (in_array(gettype($claims->nbf), ['integer', 'double']) && ($claims->nbf <= time() + $this->getLeeway())))
-                    && (!isset($claims->at_hash) || !isset($accessToken) || $claims->at_hash === $expected_at_hash)
-                );
-            }
-        };
+            $jwksProviderBuilder = (new JwksProviderBuilder())
+                ->setCache($cache)
+                ->setCacheTtl(60 * 60); // Cache JWKS for 1 hour
+
+            $issuer = (new IssuerBuilder())
+                ->setMetadataProviderBuilder($metadataProviderBuilder)
+                ->setJwksProviderBuilder($jwksProviderBuilder)
+                ->build(config('eideasy.api_url') . '/.well-known/openid-configuration');
+
+            $clientMetadata = ClientMetadata::fromArray([
+                'client_id' => config('eideasy.client_id'),
+                'client_secret' => config('eideasy.secret'),
+                'token_endpoint_auth_method' => 'client_secret_basic', // the auth method tor the token endpoint
+                'redirect_uris' => [
+                    URL::route('oidc.callback'),
+                ],
+            ]);
+
+            $client = (new ClientBuilder())
+                ->setIssuer($issuer)
+                ->setClientMetadata($clientMetadata)
+                ->build();
+
+            return $client;
+        });
+
+//        $middleware = new SessionCookieMiddleware();
+//        $authorizationService = (new AuthorizationServiceBuilder())->build();
+//        $this->app->instance(AuthorizationServiceBuilder::class, $authorizationService);
+//        $middleware = new AuthRedirectHandler($authorizationService);
     }
 }
