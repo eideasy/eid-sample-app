8b597434fa6c82a71e59ff9897cc5b834f580982
BanalitoRaulito <raul.volmar@eidasy.com>
2024-01-22 16:03:24 +0200

    feat: Remove embedded identity demo from sample app

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@getWelcome');
 
 Route::get('/sign-locally-sample', 'SampleViewController@signLocallySample');
-Route::get('/embedded-identification', 'SampleViewController@getEmbeddedIdentification');
 
 Route::get('/login-widget', 'SampleViewController@loginWidget');
 
